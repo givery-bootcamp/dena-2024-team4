8d47006bab69360f3b7041cdbaebdaa0a4153296
cookie-gg <cookie.gg.dev@gmail.com>
2024-05-28 04:39:10 +0000

    chore: Remove unused files and update gitignore patterns

--- backend/main.go
+++ backend/main.go
@@ -1,29 +1,21 @@
 package main
 
 import (
-	"context"
-	"log"
-	"myapp/ent"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"myapp/internal/config"
+	"myapp/internal/external"
+	"myapp/internal/middleware"
 )
 
 func main() {
 	// Initialize database
-	// external.SetupDB()
+	external.SetupDB()
 
-	// // Setup webserver
-	// app := gin.Default()
-	// app.Use(middleware.Transaction())
-	// app.Use(middleware.Cors())
-	// middleware.SetupRoutes(app)
-	// app.Run(fmt.Sprintf("%s:%d", config.HostName, config.Port))
-
-	client, err := ent.Open("mysql", "<user>:<pass>@tcp(<host>:<port>)/<database>?parseTime=True")
-	if err != nil {
-		log.Fatalf("failed opening connection to mysql: %v", err)
-	}
-	defer client.Close()
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
+	// Setup webserver
+	app := gin.Default()
+	app.Use(middleware.Transaction())
+	app.Use(middleware.Cors())
+	middleware.SetupRoutes(app)
+	app.Run(fmt.Sprintf("%s:%d", config.HostName, config.Port))
 }
