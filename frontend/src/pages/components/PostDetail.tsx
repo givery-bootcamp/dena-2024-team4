074558656a12eba369dcb230a160526de4e14e89
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-17 15:24:57 +0900

    feat: Postモックを実装

---
+++ frontend/src/pages/components/PostDetail.tsx
@@ -0,0 +1,17 @@
+const MOCK_POST = {
+  id: 1,
+  user_id: 1,
+  title: '私の戦闘力は53万です',
+  body: 'ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……',
+  created_at: '2021-01-01 00:00:00',
+  updated_at: '2021-01-01 00:00:00',
+  deleted_at: null,
+  user: {
+    id: 1,
+    name: 'フリーザ',
+    password: 'password',
+    created_at: '2021-01-01 00:00:00',
+    updated_at: '2021-01-01 00:00:00',
+    deleted_at: null,
+  }
+}
