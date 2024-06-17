589d7ed2060d70ef623182762c5b96b63f86eb1a
naoki-yahata <naoki.yahata@dena.com>
2024-06-17 08:47:56 +0000

    Inputに文字が入力されていないときはポストするボタンを押せないように修正

--- frontend/src/components/PostField.tsx
+++ frontend/src/components/PostField.tsx
@@ -1,7 +1,22 @@
 import { Input } from '@yamada-ui/react';
+import { ChangeEvent } from 'react';
 
-const PostField = () => {
-  return <Input placeholder="いまどうしてる？" size={'lg'} />;
+interface PostFieldProps {
+  onInputChange: (value: string) => void;
+}
+
+const PostField: React.FC<PostFieldProps> = ({ onInputChange }) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onInputChange(event.target.value);
+  };
+
+  return (
+    <Input 
+      placeholder="いまどうしてる？"
+      size={'lg'}
+      onChange={handleChange}
+    />
+  );
 };
 
 export default PostField;
--- frontend/src/components/PostForm.tsx
+++ frontend/src/components/PostForm.tsx
@@ -1,18 +1,27 @@
-import { Avatar, Button, Flex, HStack } from '@yamada-ui/react';
+import { Avatar, Button, Flex, HStack, VStack } from '@yamada-ui/react';
 import PostField from './PostField';
+import { useCallback, useState } from 'react';
 
 const PostForm = () => {
+  const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = useCallback((value: string) => {
+      setInputValue(value);
+    }, []);
+
   return (
     <div>
-      <HStack>
-        <Avatar size="md" name="Hirotomo Yamada" />
-        <PostField />
-      </HStack>
-      <Flex justify={'right'} mt={8}>
-        <Button colorScheme="sky" rounded="24px">
-          ポストする
-        </Button>
-      </Flex>
+      <VStack px={8}>
+        <HStack mt={12}>
+          <Avatar size="md" name="Hirotomo Yamada" />
+          <PostField onInputChange={handleInputChange} />
+        </HStack>
+        <Flex justify={'right'} my={8}>
+          <Button colorScheme="sky" rounded="24px" isDisabled={!inputValue}>
+            ポストする
+          </Button>
+        </Flex>
+      </VStack>
     </div>
   );
 };
