b5a3aa49f49eeb2fd4670f1e091cbd165982f653
aohana-AO <aoihana1130@gmail.com>
2024-06-17 08:26:44 +0000

    コントローラーにクエリパラメータ会得を追加

--- backend/internal/controllers/all_tweets_controller.go
+++ backend/internal/controllers/all_tweets_controller.go
@@ -5,17 +5,26 @@ import (
 	"myapp/internal/usecases"
 
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func AllTweets(ctx *gin.Context) {
-	usecase := usecases.NewAllTweetsUsecase()
+	// クエリパラメータからoffsetとlimitを取得
+	offsetStr := ctx.Query("offset")
+	limitStr := ctx.Query("limit")
 
-	result, err := usecase.Execute()
+	// 文字列を整数に変換（エラー処理は省略）
+	offset, _ := strconv.Atoi(offsetStr)
+	limit, _ := strconv.Atoi(limitStr)
+
+	usecase := usecases.NewAllTweetsUsecase(offset, limit)
+
+	result, err := usecase.Execute(offset, limit)
 	if err != nil {
 		handleError(ctx, 500, err)
 	} else if result != nil {
 		ctx.JSON(200, result)
 	} else {
-		handleError(ctx, 404, errors.New("Not found"))
+		handleError(ctx, 404, errors.New("not found"))
 	}
 }
