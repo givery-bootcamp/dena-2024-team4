aae4a166ae490e577fd0a7e75d927aae413368d9
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-18 14:57:27 +0900

    feat: SidebarPropsを実装

--- frontend/src/pages/components/Sidebar.tsx
+++ frontend/src/pages/components/Sidebar.tsx
@@ -1,13 +1,20 @@
-import { Button, Divider, HStack, Icon, VStack } from "@yamada-ui/react";
+import { Button, Divider, Flex, HStack, Icon, VStack } from "@yamada-ui/react";
 import { FaRegUser } from "react-icons/fa6";
 import { FaHouse } from "react-icons/fa6";
 
-export default function Sidebar() {
+type SidebarProps = {
+  children: React.ReactNode;
+};
+
+export default function Sidebar({ children }: SidebarProps) {
   return (
-    <VStack as="nav" w="300px" h="100vh" padding="md" bg="gray.50">
-      <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaHouse} />} p="md" rounded="40" w="fit-content">ホーム</Button>
-      <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaRegUser} />} p="md" rounded="40" w="fit-content">プロフィール</Button>
-      <Button fontSize="lg" p="md" rounded="40" bg="sky.400">ポストする</Button>
-    </VStack>
+    <Flex w="full" flexDirection="row" overflow="hidden">
+      <VStack as="nav" w="300px" h="100vh" padding="md" bg="gray.50">
+        <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaHouse} />} p="md" rounded="40" w="fit-content">ホーム</Button>
+        <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaRegUser} />} p="md" rounded="40" w="fit-content">プロフィール</Button>
+        <Button fontSize="lg" p="md" rounded="40" bg="sky.400">ポストする</Button>
+      </VStack>
+      {children}
+    </Flex>
   )
 }
