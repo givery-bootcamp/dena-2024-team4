18dcfdd18b35313085ad20aefb376932a2923f43
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-18 16:41:45 +0900

    fix: SidebarItemをPropsで受け取れるように修正

--- frontend/src/pages/components/Sidebar.tsx
+++ frontend/src/pages/components/Sidebar.tsx
@@ -1,17 +1,24 @@
 import { Button, Divider, Flex, HStack, Icon, VStack } from "@yamada-ui/react";
-import { FaRegUser } from "react-icons/fa6";
-import { FaHouse } from "react-icons/fa6";
+import { IconType } from "react-icons";
 
 type SidebarProps = {
+  items: SidebarItem[];
   children: React.ReactNode;
 };
 
-export default function Sidebar({ children }: SidebarProps) {
+type SidebarItem = {
+  title: string,
+  icon: IconType,
+  action: () => void,
+};
+
+export default function Sidebar({ items, children }: SidebarProps) {
   return (
     <Flex w="full" flexDirection="row" overflow="hidden">
       <VStack as="nav" w="300px" h="100vh" padding="md" bg="gray.50">
-        <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaHouse} />} p="md" rounded="40" w="fit-content">ホーム</Button>
-        <Button variant="ghost" fontSize="lg" leftIcon={<Icon as={FaRegUser} />} p="md" rounded="40" w="fit-content">プロフィール</Button>
+        {items.map((item, index) => (
+          <Button key={index} variant="ghost" fontSize="lg" leftIcon={<Icon as={item.icon} />} p="md" rounded="40" w="fit-content" onClick={item.action}>{item.title}</Button>
+        ))}
         <Button fontSize="lg" p="md" rounded="40" bg="sky.400">ポストする</Button>
       </VStack>
       {children}
