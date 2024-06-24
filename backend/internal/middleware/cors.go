7e91ad2a0cd8f4a98d42e7d28c6c53426907885c
ktaroabobon <ktaroabobon@gmail.com>
2024-06-24 16:37:05 +0900

    Add: swagger

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,14 +1,15 @@
 package middleware
 
 import (
+	myConfig "myapp/internal/config"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	myConfig "myapp/internal/config"
 )
 
 func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{myConfig.CorsAllowOrigin}
+	config.AllowOrigins = []string{myConfig.CorsAllowOrigin, myConfig.CorsAllowOriginSwagger}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
