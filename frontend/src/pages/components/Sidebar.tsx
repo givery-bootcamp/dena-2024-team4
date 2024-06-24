c65572594d86d6eb4e8c8f86100556191fbd1ab6
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-24 14:04:08 +0900

    feat: Sidebarがコンテンツのスクロールに追跡するように変更

--- frontend/src/pages/components/Sidebar.tsx
+++ frontend/src/pages/components/Sidebar.tsx
@@ -28,8 +28,8 @@ const items: SidebarItem[] = [
 
 export default function Sidebar({ children }: SidebarProps) {
   return (
-    <Flex w="full" flexDirection="row" overflow="hidden">
-      <VStack as="nav" w="300px" h="100vh" padding="md" bg="gray.50">
+    <Flex w="full" flexDirection="row">
+      <VStack w="300px" h="100vh" padding="md" position="sticky" top="0">
         {items.map((item, index) => (
           <Button key={index} variant="ghost" fontSize="lg" leftIcon={<Icon as={item.icon} />} p="md" rounded="40" w="fit-content" onClick={item.action}>{item.title}</Button>
         ))}
