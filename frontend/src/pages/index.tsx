336da494603fd3fec170a68c6b8f205d0980385b
naoki-yahata <naoki.yahata@dena.com>
2024-06-26 20:13:24 +0900

    chore: デバッグのための記述を削除

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,15 @@
 import Head from 'next/head';
 import { getServerSideCommonProps } from '@/libs/yamada-ui';
-import { useDisclosure } from '@yamada-ui/react';
 
 export const getServerSideProps = getServerSideCommonProps;
 
 export default function Home() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <>
       <Head>
         <title>Clone Twitter</title>
       </Head>
-      <main>
-      </main>
+      <main></main>
     </>
   );
 }
