9855b5bd3983481db37b8b52bb43a9ee7e71c7ab
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-28 11:04:15 +0900

    feat: credentialsのコメントアウトを削除

--- frontend/src/features/apis/updatePost.ts
+++ frontend/src/features/apis/updatePost.ts
@@ -1,6 +1,5 @@
 export const updatePost = async (postId: number, data: any) =>
   fetch(`${process.env.NEXT_PUBLIC_API_URL}/tweets/${postId}`, {
     method: 'put',
-    // credentials: 'include',
     body: JSON.stringify(data),
   });
