b86ab6ed9290ad7ae6b6c3475570edf8bc942b54
shogo.takasaki <stakasaki707@gmail.com>
2024-06-28 11:39:49 +0900

    add auth to tweets endpoint

--- backend/internal/middleware/router.go
+++ backend/internal/middleware/router.go
@@ -20,11 +20,10 @@ func SetupRoutes(app *gin.Engine) {
 	auth := app.Group("/")
 	auth.Use(AuthRequired())
 	{
-		auth.GET("/user", controllers.AuthUser)
+		auth.GET("/user", controllers.AuthUser)                  // ログイン中のユーザー情報を取得
+		auth.GET("/tweets", controllers.AllTweets)               // AllTweets の関数を直接呼び出す
+		auth.POST("/tweets", controllers.PostTweetDetail)        // ツイートを投稿
+		auth.GET("/tweets/:tweetId", controllers.TweetDetail)    // tweetIdでツイートの詳細を取得
+		auth.PUT("/tweets/:tweetId", controllers.PutTweetDetail) // ツイートを更新
 	}
-
-	// AllTweets の関数を直接呼び出す
-	app.GET("/tweets", controllers.AllTweets)
-	app.GET("/tweets/:tweetId", controllers.TweetDetail) // tweetIdでツイートの詳細を取得
-	app.POST("/tweets", controllers.PostTweetDetail)     // ツイートを投稿
 }
