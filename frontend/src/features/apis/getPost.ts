ccfae6923076ca1cb2cce2bf190b67a1c7efc177
SNQ2-2001 <taishin.miyamoto@dena.com>
2024-06-28 16:50:12 +0900

    feat: credentialsを追加

--- frontend/src/features/apis/getPost.ts
+++ frontend/src/features/apis/getPost.ts
@@ -1,5 +1,4 @@
 export const getPostFetcher = (postId: number) =>
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/tweets/${postId}`).then((res) =>
-      res.json(),
-    );
-  
+  fetch(`${process.env.NEXT_PUBLIC_API_URL}/tweets/${postId}`, {
+    credentials: 'include',
+  }).then((res) => res.json());
