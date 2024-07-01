bd41cc98bc75644be7f00647bd3a6e683c19df94
cookie-gg <cookie.gg.dev@gmail.com>
2024-07-01 10:32:44 +0900

    chore: Update verifyUser middleware to use getCookies
    This commit updates the `verifyUser` middleware in the `frontend/src/middlewares/verifyUser.ts` file. It replaces the usage of the `getCookie` function with the `getCookies` function from the `cookies-next` library. It also adds a console log to display the cookies. This change was made to improve the functionality of the middleware.

--- frontend/src/middlewares/verifyUser.ts
+++ frontend/src/middlewares/verifyUser.ts
@@ -1,7 +1,12 @@
-import { getCookie } from 'cookies-next';
+import { getCookie, getCookies } from 'cookies-next';
 import { GetServerSideProps } from 'next';
 
 export const verifyUser: GetServerSideProps = async ({ req, res }) => {
+  const cookies = getCookies({
+    req,
+    res,
+  });
+  console.log(cookies);
   const jwt = getCookie('jwt', { req, res });
 
   if (!jwt)
